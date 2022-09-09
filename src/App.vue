1a612326ac8b8ada965c013dc5c61e823c6cea1f
Uzair <uzairsarvan@gmail.com>
2022-09-09 13:12:22 +0200

    Finished Woking on displaying components

--- src/App.vue
+++ src/App.vue
@@ -1,144 +1,26 @@
 <template>
-  <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text text-3xl md: text-5xl font-bold mb-3">
-      <img class="inline-block mx-auto align-middle text-right" src="./img/Logo.svg" alt="">
-      Aspect Pc Builders</div>
-  </header>
-  <div class="container ">
-    <h1 class="text-3xl text-red-500">Hello World</h1>
-    <body class="bg-white w-full h-screen text-black" > 
-  <h2 class="font-bold">Case</h2>
-  <div class="flex space-x-4 items-baseline justify-start">
-  <img src="https://img.icons8.com/sf-regular/96/000000/workstation.png"/>
-  <div>
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/71kbyr6phxl._sl1300__1024x768_.jpg" alt="">
-    <div>
-      <h2 class="text-center">
-      Cooler Master 
-      MCW-L3B2-KN5N MasterBox 
-      Lite 3 Micro-ATX Chassis</h2>
-      <h3 class="text-red-600 text-center">R599,00</h3>
-    </div>
-  
-  </div>
-  <div>
-    <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/phanteks-ph-ec300atg_bk01-eclipse-p300a-tg-black-steel-atx-mid-tower-1.jpg" alt="">
-    <div>
-      <h2 class="text-center">
-        Phanteks PH-EC300ATG_BK01 Eclipse P300A Tempered Glass Black Steel ATX Mid Tower Desktop Chassis</h2>
-      <h3 class="text-red-600 text-center">R1,249.00 </h3>
-    </div>
-  </div>
-  <div>
-    <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/p400a-digital-rgb-black_1_.jpg" alt="">
-    <div>
-      <h2 class="text-center">
-        Phanteks PH-EC400ATG_DBK01 Eclipse P400A Digital RGB Tempered Glass Satin Black Steel ATX Mid Tower Desktop Chassis</h2>
-      <h3 class="text-red-600 text-center">R1,499.00 </h3>
-    </div>
-  </div>
-  
-  <div>
-    <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/phanteks-ph-ec500atg_dbk-eclipse-p500a-drgb-tg-black-e-atx-mid-tower-1.jpg" alt="">
-    <div>
-      <h2 class="text-center">
-        Phanteks PH-EC500ATG_DBK Eclipse P500A DRGB Tempered Glass Black E-ATX Mid-Tower Desktop Chassis</h2>
-      <h3 class="text-red-600 text-center">R2,399.00</h3>
-    </div>
-  </div>
-  
-
-  </div>
- 
-  <h2 class="font-bold">CPU</h2>
-  <div class="flex space-x-4 items-baseline justify-start">
-    <img src="https://img.icons8.com/sf-regular/96/000000/processor.png"/>
-    <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/intel-alder-lake-corei3-box_1__1.jpg" alt="">
-    <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/intel-12gen-i5-stndbox-01.jpg" alt="">
-    <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/intel-12gen-i7-stndbox-01_1.jpg" alt="">
-    <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/intel-12gen-i9-12900k-box-01_1.jpg" alt="">
-  </div>
-
-  <h2 class="font-bold">Motherboard</h2>
-  <div class="flex space-x-4 items-baseline justify-start">
-  <img src="https://img.icons8.com/sf-regular/96/000000/motherboard.png"/>
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/gigabyte_h610m_h_ddr4_01-ra57tzgs.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/GigabyteB660.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/h670pwifid401.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/ga-z690m-ds3h-ddr4_1_.jpg" alt="">
-  </div>
-
-  <h2 class="font-bold">Ram</h2>
-  <div class="flex space-x-4 items-baseline justify-start">
-  <img src="https://img.icons8.com/sf-regular/96/000000/memory-slot.png"/>
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/RipjawsV8GB.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/VengeanceLPX16GB.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/HyperXFury32GB.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/HyperXFuryRGB64GB.jpg" alt="">
-  </div>
-    
-   <h2 class="font-bold">Graphics Card</h2>
-   <div class="flex space-x-4 items-baseline justify-start">
-  <img src="https://img.icons8.com/sf-regular/96/000000/video-card.png"/>
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/3050-ventus-2x-8g.png" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/nvidia-geforce-rtx-3060-ti.png" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/3070-ti-back-300x198.png" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/nvidia-geforce-rtx-3090-ti.png" alt="">
-  </div>
-   
-  <h2 class="font-bold">Solid State Drive</h2>
-  <div class="flex space-x-4 items-baseline justify-start">
-  <img src="https://img.icons8.com/sf-regular/96/000000/ssd.png"/>
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/CrucialCT240BX500SSD1BX500240GB.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/CrucialCT480BX500SSD1BX500480GB.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/CrucialCT500P2SSD8P2500GB.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/CrucialCT1000MX500SSD1MX5001TB.jpg" alt="">
-  </div>
-  
-  <h2 class="font-bold">Hard Drive</h2>
-  <div class="flex space-x-4 items-baseline justify-start">
-    <img src="https://img.icons8.com/sf-regular/96/000000/hdd.png"/>
-    <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/wd5000lpsx_01-uvvaizgs.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/wd_blue_3tb_01.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/wd_blue_3tb_01.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/western-digital-wd40ezaz-01.jpg" alt="">
-  </div>
-
-  <h2 class="font-bold">Power Supply</h2>
-  <div class="flex space-x-4 items-baseline justify-start">
-  <img src="https://img.icons8.com/sf-regular/96/000000/lightning-bolt.png"/>
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/CorsairCP9020210CVSeries550W80PlusBronzeCertifiedNonModularBlackDesktopPowerSupply.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/CorsairCP9020221CX650M650W80PlusBronzeSemiModularBlackDesktopPowerSupply.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/CorsairCP9020138HX850850W80PlusPlatinumCertifiedFullyModularDesktopPowerSupply.jpg" alt="">
-  <img class="-mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 " src="./img/CorsairCP9020139HX10001000W80PlusPlatinumCertifiedFullyModularDesktopPowerSupply.jpg" alt="">
-  </div>
- 
-</body>
-
-
-  </div>
-  <footer class="text-gray-100 bg-gray-800">
-    <div class="max-w-3xl mx-auto py-6">
-        <h1 class="text-center text-lg lg:text-2xl">
-           Thanks for building your Pc
-        </h1>
-        <hr class="h-px mt-6 bg-gray-700 border-none">
-
-        <div class="flex flex-col items-center justify-between mt-6 md:flex-row">
-            <div>
-                <a href="#" class="text-xl font-bold text-gray-100 hover:text-gray-400">Uzair Sarvan</a>
-            </div>
-
-            <div class="flex mt-4 md:m-0">
-                <div class="-mx-4">
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
-<style src="./assets/tailwind.css"></style >
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
